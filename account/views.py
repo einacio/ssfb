6673f060b760523c2734febeedf22ab5020ab670
Einacio <pablo@loop-sistemas.com.ar>
2019-06-10 15:33:31 +0200

    connect app
    initial views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .models import User, UserHistory
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    request.session['is_logged_in'] = not request.session.get('is_logged_in')
+    return render(request, 'account/index.html')
