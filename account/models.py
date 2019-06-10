ee61897e372afdcabf92e21fc4c3b689b94a86ed
Einacio <pablo@loop-sistemas.com.ar>
2019-06-10 13:44:46 +0200

    first models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField()
+    fb_user_id = models.CharField(max_length=50)
+    fb_token = models.TextField()
+
+
+class UserHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    action = models.BooleanField(help_text='True signup/login, False deauth')
+    datetime = models.DateTimeField()
